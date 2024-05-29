a763677c3c08ff16fca20b70fb6ea95a5fd2ca0c
Sylvan Ash <sylvan.d.ash@gmail.com>
2024-05-29 14:39:56 +0300

    Initial Commit

---
+++ Fancy Diner/Fancy_DinerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fancy_DinerApp.swift
+//  Fancy Diner
+//
+//  Created by Sylvan Ash on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Fancy_DinerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
