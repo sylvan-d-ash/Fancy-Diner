8a56a7046c5e9f449f10fff6d032225829feb619
Sylvan Ash <sylvan.d.ash@gmail.com>
2024-06-13 12:52:36 +0300

    Show total price + tip + show an alert on order

--- Fancy Diner/CheckoutView.swift
+++ Fancy Diner/CheckoutView.swift
@@ -19,9 +19,16 @@ struct CheckoutView: View {
     @State private var addLoyaltyPoints = false
     @State private var loyaltyNumber = ""
     @State private var tipAmount = 10
+    @State private var isPaymentAlertShown = false
 
     private let paymentTypes: [PaymentType] = PaymentType.allCases
     private let tipAmounts = [0, 10, 15, 20, 25]
+
+    private var totalPrice: String {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+        return (total + tipValue).formatted(.currency(code: "USD"))
+    }
 
     var body: some View {
         Form {
@@ -48,14 +55,19 @@ struct CheckoutView: View {
                 .pickerStyle(.segmented)
             }
 
-            Section("Total: $100") {
+            Section("Total: \(totalPrice)") {
                 Button("Confirm order") {
-                    //
+                    isPaymentAlertShown.toggle()
                 }
             }
         }
         .navigationTitle("Payment")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Order Confirmed", isPresented: $isPaymentAlertShown) {
+            // buttons go here
+        } message: {
+            Text("Your total was \(totalPrice) - thank you!")
+        }
     }
 }
 
